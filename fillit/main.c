701a8035fa1fd26bb1c89298f0a622ce61aebd84
Mykhaylo VASKIV <mvaskiv@e1r5p8.unit.ua>
2017-11-29 11:06:20 +0200

    update

--- fillit/main.c
+++ fillit/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vblokha <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: vblokha <vblokha@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/29 10:26:08 by vblokha           #+#    #+#             */
-/*   Updated: 2017/11/29 10:26:09 by vblokha          ###   ########.fr       */
+/*   Updated: 2017/11/29 11:05:56 by mvaskiv          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,13 @@ int		main(int argc, char **argv)
 
 	if (argc != 2)
 	{
-		ft_putendl("usage [map]");
+		ft_putendl("usage: fillit tetriminos_list");
 		return (0);
 	}
 	fd = open(argv[1], O_RDONLY);
 	input = ft_copy_input(fd);
 	close(fd);
-	if (!validation_for_each(input))
+	if (!ft_val_for_each(input))
 	{
 		ft_putendl("error");
 		return (0);
--- fillit/validate_me.c
+++ fillit/validate_me.c
@@ -3,50 +3,50 @@
 /*                                                        :::      ::::::::   */
 /*   validate_me.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vblokha <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: mvaskiv <mvaskiv@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/11/29 10:35:07 by vblokha           #+#    #+#             */
-/*   Updated: 2017/11/29 10:39:23 by vblokha          ###   ########.fr       */
+/*   Created: 2017/11/29 10:56:55 by mvaskiv           #+#    #+#             */
+/*   Updated: 2017/11/29 11:06:11 by mvaskiv          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-int					connections(char *s)
+int		ft_connections(char *s)
 {
-	int i;
-	int k;
+	int	i;
+	int	k;
 
 	i = 0;
 	k = 0;
 	while (s[i] && i < 20)
 	{
-		if (s[i] == '#' && s[i + 1] == '#' && i + 1 < 21)
+		if (s[i] == '#' && s[i + 1] == '#' && (i + 1) < 21)
 			k++;
-		if (s[i] == '#' && s[i - 1] == '#' && i - 1 >= 0)
+		if (s[i] == '#' && s[i - 1] == '#' && (i - 1) >= 0)
 			k++;
-		if (s[i] == '#' && s[i + 5] == '#' && i + 5 < 21)
+		if (s[i] == '#' && s[i + 5] == '#' && (i + 5) < 21)
 			k++;
-		if (s[i] == '#' && s[i - 5] == '#' && i - 5 >= 0)
+		if (s[i] == '#' && s[i - 5] == '#' && (i - 5) >= 0)
 			k++;
 		i++;
 	}
-	if (k == 8 || k == 6)
+	if (k == 8 && k == 6)
 		return (1);
 	return (0);
 }
 
-int			validation_for_one(char *s)
+int		ft_val_for_one(char *s)
 {
-	size_t i;
-	size_t h;
-	size_t p;
+	size_t	i;
+	size_t	h;
+	size_t	p;
 
 	i = 0;
 	h = 0;
 	p = 0;
 	while (((s[i] == '.' || s[i] == '#')
-		|| (s[i % 5] == '\n' && i % 5 == 4)) && i < 21)
+		|| (s[i % 5] == '\n' && (i % 5) == 4)) && i < 21)
 	{
 		if (h == 5)
 			return (0);
@@ -57,18 +57,20 @@ int			validation_for_one(char *s)
 		i++;
 	}
 	return ((h == 4 && p == 12 && (s[i] == '\n'
-		|| s[i] == '\0') && connections(s) == 1) ? 1 : 0);
+		|| s[i] == '\0') && ft_connections(s) == 1) ? 1 : 0);
 }
 
-int			validation_for_each (char *s)
+int		ft_val_for_each(char *s)
 {
-	size_t i;
+	size_t	i;
 
 	i = 0;
 	while (s[i])
-		if (validation_for_one(s + i) == 1)
-			i = i + 21;
+	{
+		if (ft_val_for_one(s + i) == 1)
+			i += 21;
 		else
 			return (0);
+	}
 	return (1);
 }
