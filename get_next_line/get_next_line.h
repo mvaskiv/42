36b596e08e54f44f2eab1d948f6adf92e35e7651
mvaskiv <mike@vaskiv.com>
2017-12-25 16:36:17 +0200

    upd

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -7,13 +7,6 @@
 
 # define BUFF_SIZE 32
 
-typedef struct		s_line
-{
-	void									*content;
-	unsigned int					file;
-	struct s_line					*next;
-}									t_line;
-
 int		get_next_line(const int fd, char **line);
 
 #endif
