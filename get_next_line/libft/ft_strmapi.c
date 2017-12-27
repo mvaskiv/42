c73ef6602c24cdefbe4ad2cc669fe9a6dd754960
Mykhaylo VASKIV <mvaskiv@e1r5p8.unit.ua>
2017-12-27 08:40:08 +0200

    update

---
+++ get_next_line/libft/ft_strmapi.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mvaskiv <mvaskiv@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/14 16:06:51 by mvaskiv           #+#    #+#             */
+/*   Updated: 2017/12/27 08:33:04 by mvaskiv          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char		*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char				*str;
+	unsigned int		i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i])
+		i++;
+	if (!(str = (char *)malloc(i + 1)))
+		return (NULL);
+	i = 0;
+	while (str[i])
+		((char *)str)[i++] = '\0';
+	i = 0;
+	if (s && f)
+	{
+		while (s[i])
+		{
+			str[i] = f(i, s[i]);
+			i++;
+		}
+		str[i] = '\0';
+		return (str);
+	}
+	return (NULL);
+}
