319d8eeb4dcffb73d000cb89baa42b578403410c
Mykhaylo VASKIV <mvaskiv@e1r4p7.unit.ua>
2018-01-17 20:53:54 +0200

    still working on it

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,35 +1,24 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mvaskiv <marvin@42.fr>                     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/12/26 14:19:09 by mvaskiv           #+#    #+#             */
-/*   Updated: 2017/12/26 14:19:13 by mvaskiv          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static char	*ft_cat(char *buff, char *tab)
+static char	*ft_overwrite(char *buff, char *tmp)
 {
-	unsigned long	i;
-	unsigned long	j;
+	unsigned long i;
+	unsigned long j;
 	char			*str;
 
 	i = 0;
 	j = 0;
 	if (buff)
 		i = ft_strlen(buff);
-	if (tab)
-		j = ft_strlen(tab);
-	str = (char *)malloc(sizeof(*str) * (i + j + 1));
+	if (tmp)
+		j = ft_strlen(tmp);
+	str = (char *)malloc(sizeof(char) * (i + j + 1));
 	ft_memcpy(str, buff, i);
-	ft_memcpy(str + i, tab, j);
+	ft_memcpy(str + i, tmp, j);
 	str[i + j] = '\0';
 	free(buff);
-	ft_bzero(tab, BUFF_SIZE + 1);
+	ft_bzero(tmp, BUFF_SIZE + 1);
 	return (str);
 }
 
@@ -38,7 +27,7 @@ static int	ft_endl(char *buff)
 	int		i;
 
 	i = 0;
-	while (buff[i] != '\n' && buff[i])
+	while ((buff[i] != '\n') && buff[i])
 		i++;
 	if (buff[i] == '\n')
 	{
@@ -49,62 +38,68 @@ static int	ft_endl(char *buff)
 		return (-1);
 }
 
-static int	ft_check(char **buff, char **tab, char **line)
+static int	ft_check(char **buff, char **tmp, char **line)
 {
-	char	*str;
 	int		final;
 
-	*buff = ft_cat(*buff, *tab);
+	*buff = ft_overwrite(*buff, *tmp);
 	final = ft_endl(*buff);
 	if (final > -1)
 	{
 		*line = ft_strdup(*buff);
-		str = *buff;
 		*buff = ft_strdup(*buff + final + 1);
-		free(str);
 		return (1);
 	}
 	return (0);
 }
 
-int			get_next_line(const int fd, char **line)
+static char		**ft_setthings(char **buff, int fd, char **tmp)
 {
-	static char *buff[12000];
+	*tmp = ft_strnew(BUFF_SIZE);
+	if (!buff)
+		buff = (char**)malloc(sizeof(int) * fd * 100);
+	return (buff);
+}
+
+int		get_next_line(const int fd, char **line)
+{
+	static char **buff;
 	char		*tmp;
 	int			ret;
-	int			o;
+	int			output;
 
-	tmp = ft_strnew(BUFF_SIZE);
+	buff = ft_setthings(buff, fd, &tmp);
 	if (!line || BUFF_SIZE <= 0 || fd < 0 || (ret = read(fd, tmp, 0)) < 0)
 		return (-1);
 	while ((ret = read(fd, tmp, BUFF_SIZE)) > 0)
 	{
-		o = ft_check(&buff[fd], &tmp, line);
+		output = ft_check(&buff[fd], &tmp, line);
 		free(tmp);
-		if (o == 1)
+		if (output == 1)
 			return (1);
 		tmp = ft_strnew(BUFF_SIZE);
 	}
-	if ((o = ft_check(&buff[fd], &tmp, line)))
+	if ((output = ft_check(&buff[fd], &tmp, line)))
 		return (1);
-	else if (ft_strlen(buff[fd]) > 0)
+	else if (ft_strlen(buff[fd]) > 1)
 	{
 		*line = ft_strdup(buff[fd]);
 		ft_strdel(&buff[fd]);
 		return (1);
 	}
-	return (o);
+	return (output);
 }
 
 int		main(int argc, char **argv)
 {
-	int fd1;
-	int fd2;
+	int fd;
+	int ret;
 	char *line;
 
-	fd1 = open(argv[1], O_RDONLY);
-	fd2 = open(argv[2], O_RDONLY);
-	get_next_line(fd1, &line);
+	fd = open(argv[1], O_RDONLY);
+	while ((ret = get_next_line(fd, &line)) > 0)
 		ft_putendl(line);
+	close(fd);
+	free(line);
 	return (0);
 }
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mvaskiv <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: mvaskiv <mvaskiv@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/26 14:19:17 by mvaskiv           #+#    #+#             */
-/*   Updated: 2017/12/26 14:19:19 by mvaskiv          ###   ########.fr       */
+/*   Updated: 2018/01/17 20:46:44 by mvaskiv          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-#	include "libft/includes/libft.h"
+# include "libft/includes/libft.h"
 # include <fcntl.h>
 
 # define BUFF_SIZE 100
