d9f4fe93e993ba5e266d6aeb695d848a8b0d79aa
mvaskiv <mike@vaskiv.com>
2018-03-11 15:16:53 +0200

    Norme almost fixed
    need to check -t

--- ft_ls/ft_write_stats.c
+++ ft_ls/ft_write_stats.c
@@ -6,7 +6,7 @@
 /*   By: mvaskiv <mvaskiv@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/11 14:11:02 by mvaskiv           #+#    #+#             */
-/*   Updated: 2018/03/11 15:07:45 by mvaskiv          ###   ########.fr       */
+/*   Updated: 2018/03/11 15:11:43 by mvaskiv          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ int			ft_scan_flags(t_flags *flags, char **arg, int argc)
 				flags->a = ft_strchr(arg[i], 'a') ? 1 : flags->a;
 				flags->l = ft_strchr(arg[i], 'l') ? 1 : flags->l;
 				flags->r = ft_strchr(arg[i], 'r') ? 1 : flags->r;
-				flags->rh = ft_strchr(arg[i], 'R') ? 1 : flags->R;
+				flags->rh = ft_strchr(arg[i], 'R') ? 1 : flags->rh;
 				flags->t = ft_strchr(arg[i], 't') ? 1 : flags->t;
 			}
 			i++;
