63ff5d26e6af4aaad34183c6df4405f21d4205ef
mvaskiv <mike@vaskiv.com>
2018-03-15 18:28:59 +0200

    minishell cd - need to fix error handler

--- minishell/ft_builtins.c
+++ minishell/ft_builtins.c
@@ -6,7 +6,7 @@
 /*   By: mvaskiv <mvaskiv@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/15 12:33:33 by mvaskiv           #+#    #+#             */
-/*   Updated: 2018/03/15 18:22:30 by mvaskiv          ###   ########.fr       */
+/*   Updated: 2018/03/15 18:25:51 by mvaskiv          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,21 +21,23 @@ int		ft_cd(char *line, char **name)
 	if (!line[3])
 	{
 		chdir(getenv("HOME"));
+		*name = ft_strdup("Sweet_Home");
 		return (1);
 	}
 	stat(ft_strjoin(ft_strjoin(getenv("PWD"), "/"), line + 3), &stats);
-	if (!(S_ISDIR(stats.st_mode)))
+	if ((chdir(ft_strdup(line + 3))) == 0)
+		chdir(ft_strdup(line + 3));
+	else if (!(S_ISDIR(stats.st_mode)))
 	{
 		ft_mini_printf("cd: not a directory: %s\n", line + 3);
 		return (1);
 	}
-	else if ((chdir(ft_strdup(line + 3))) == -1)
+//	if ((chdir(ft_strdup(line + 3))) == -1)
+	else
 	{
 		ft_mini_printf("cd: no such file or directory: %s\n", line + 3);
 		return (1);
 	}
-	else
-		chdir(ft_strdup(line + 3));
 	*name = ft_strjoin("..", ft_strrchr(getcwd(dir, NULL), '/'));
 	ft_strdel(&dir);
 	return (1);
