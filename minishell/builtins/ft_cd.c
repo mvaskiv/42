9cdfc908bbf0f48cf6d75be1028c091c3eb737c5
mvaskiv <mike@vaskiv.com>
2018-03-17 19:18:39 +0200

    minishell - Sweet_Lemonade
    refactored

---
+++ minishell/builtins/ft_cd.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_cd.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mvaskiv <mvaskiv@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/03/15 18:51:40 by mvaskiv           #+#    #+#             */
+/*   Updated: 2018/03/17 18:22:04 by mvaskiv          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+static int	ft_cd_error(const char *line)
+{
+	struct stat	stats;
+
+	if ((stat(ft_strjoin(ft_strjoin(getenv("PWD"), "/"), line + 3), &stats)))
+	{
+		ft_mini_printf("cd: no such file or directory: %s\n", line + 3);
+		return (1);
+	}
+	else if (!(S_ISDIR(stats.st_mode)))
+	{
+		ft_mini_printf("cd: not a directory: %s\n", line + 3);
+		return (1);
+	}
+	return (0);
+}
+
+int			ft_cd(const char *line)
+{
+	char		*dir;
+	char 		*new_dir;
+
+	new_dir = ft_strdup(line + 3);
+	dir = NULL;
+	if (!line[3])
+	{
+		chdir(getenv("HOME"));
+		return (1);
+	}
+	if ((chdir(new_dir)) == 0);
+	else if (ft_cd_error(line))
+		return (1);
+	dir = getcwd(dir, 0);
+	ft_strdel(&new_dir);
+	ft_strdel(&dir);
+	return (1);
+}
---
+++ minishell/builtins/ft_setenv.c
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_setenv.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mvaskiv <mvaskiv@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/03/16 13:10:14 by mvaskiv           #+#    #+#             */
+/*   Updated: 2018/03/17 18:39:44 by mvaskiv          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+static char 	*ft_env_name(const char *line)
+{
+	char 	*name;
+	int 	i;
+	int 	j;
+
+	j = 0;
+	i = 0;
+	while (line[7 + i] != '=')
+		i++;
+	name = (char*)malloc(sizeof(char) * i + 1);
+	while (j <= i)
+	{
+		name[j] = line[7 + j];
+		j++;
+	}
+	name[j] = '\0';
+	return (name);
+}
+
+char	**ft_change_env(char ***env, const char *line, int i)
+{
+	char	**dup;
+	char 	**envp = *env;
+	int		j;
+
+	j = 0;
+	while (envp[j++] != NULL);
+	dup = (char**)malloc(sizeof(char*) * j);
+	j = 0;
+	while (envp[j] != NULL)
+	{
+		dup[j] = ft_strdup(envp[j]);
+		j++;
+		if (j == i)
+			dup[j++] = ft_strdup(line + 7);
+	}
+	dup[j] = NULL;
+	return (dup);
+}
+
+char 	**ft_new_env(char ***env, const char *line, int i)
+{
+	char 	**dup;
+	char 	**envp = *env;
+
+	dup = (char**)malloc(sizeof(char*) * i + 1);
+	i = 0;
+	while (envp[i] != NULL)
+	{
+		dup[i] = ft_strdup(envp[i]);
+		i++;
+	}
+	dup[i++] = ft_strdup(line + 7);
+	dup[i] = NULL;
+	return (dup);
+}
+
+char	**ft_setenv(char ***env, const char *line)
+{
+	int		i;
+	char	**dup;
+	char 	**envp = *env;
+	char 	*env_name;
+
+	i = 0;
+	env_name = ft_env_name(line);
+	while (envp[i] != NULL)
+	{
+		if (ft_strstr(envp[i], env_name))
+		{
+			ft_strdel(&env_name);
+			dup = ft_change_env(env, line, i);
+			ft_arrclr(*env);
+			return (dup);
+		}
+		i++;
+	}
+	dup = ft_new_env(env, line, i);
+	ft_strdel(&env_name);
+	ft_arrclr(*env);
+	return (dup);
+}
